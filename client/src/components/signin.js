bf78c3de2ebfc836eacfe402a832676dc1389ee2
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-04-16 03:39:27 +0530

    make it better

--- client/src/components/signin.js
+++ client/src/components/signin.js
@@ -5,6 +5,8 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -19,7 +21,9 @@ function Signin() {
   const [password, setPassword] = useState("");
   const [warning, setWarning] = useState(false);
   const [passWarnint, setPasswarning] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   async function signIn(useremail_phone, userpass) {
+    setIsLoading(true);
     var data;
     if (useremail_phone.includes(".com")) {
       data = {
@@ -41,6 +45,7 @@ function Signin() {
         data: data,
       });
       const responseData = await response.data.Token;
+      setIsLoading(false);
       if (responseData === "invalid") {
         setWarning(true);
         setPasswarning(true);
@@ -56,29 +61,6 @@ function Signin() {
     }
   }
 
-  const guestuser = async (guestEmail, guestPass) => {
-    try {
-      const response = await axios({
-        method: "post",
-        url: URL + "/signin",
-        headers: { "content-type": "application/json" },
-        data: { phoneNo: guestEmail, password: guestPass },
-      });
-      const responseData = await response.data.Token;
-      if (responseData === "invalid") {
-        setWarning(true);
-        setPasswarning(true);
-      } else {
-        localStorage.setItem("Token", responseData);
-        dispatch(TOKEN(responseData));
-        setphoneNo("");
-        setPassword("");
-        navigate("/");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const style = { margin: "10px  0" };
 
   return (
@@ -96,8 +78,12 @@ function Signin() {
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
+              position: "relative",
             }}
           >
+            <div style={{ position: "absolute", top: "0", right: "0" }}>
+              {isLoading ? <CircularProgress /> : " "}
+            </div>
             <div style={style}>
               <TextField
                 sx={{
