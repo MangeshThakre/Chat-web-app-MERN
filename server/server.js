1568985da98796960c86d4b2acde2f9c04d4e484
Mangesh Thakre <mangeshthakre933@gmail.com>
2022-10-31 02:55:10 +0530

    added railway.json

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use("/", router);
 //----------------------------
 const __dirname1 = path.resolve()
 if (process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(__dirname1, "/client/build")))
+app.use(express.static(path.join(__dirname1, "./client/build")));
 app.get("*",(req,res)=>{
   res.sendFile(path.resolve(__dirname1 , "client" , "build" , "index.html"))
 })
